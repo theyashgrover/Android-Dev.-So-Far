ffcad8889bc8ac19db6eed2b1b9e003e1d6b190e
Yash Grover <83108253+theyashgrover@users.noreply.github.com>
2021-08-24 09:40:49 -0700

    Commenting the code..

--- BirthdayGreet/app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
+++ BirthdayGreet/app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_birthday_greeting.*
 
-class BirthdayGreetingActivity : AppCompatActivity() {
+// class of the BirthdayGreetingAcitivity 
+class BirthdayGreetingActivity : AppCompatActivity(){
+//On create method starts here :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//The BirthdayGreetingActivity's UI is linked to the kotlin code here :
         setContentView(R.layout.activity_birthday_greeting)
+//variable defined to store the value of the key passed through intent to this activity..
         val name = intent.getStringExtra("name")
+//setting the textview's text to birthday wish with name of the person :
         birthdayGreeting.text = "Happy Birthday\n$name !!"
     }
 }
